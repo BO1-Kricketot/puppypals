35cd2f396bca6d12eb2cfc3a7308e0bce2174a20
Tom Spitz <tjspitz1986@gmail.com>
2023-03-04 20:51:17 -0600

    First commit on feature branch. Just making sure it works.

---
+++ client/features/Profile/index.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
+
+export default function Profile() {
+  return (
+    <SafeAreaView>
+      <Text>Look, it's a Profile!</Text>
+    </SafeAreaView>
+  );
+};
