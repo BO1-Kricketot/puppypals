3a46203cfef4c14a2b5a44330c5b197e2b71d80c
boltonlin <boltonlinbo@gmail.com>
2023-03-06 14:25:39 -0800

    added dog model and controller skeleton

---
+++ server/controllers/DogController.js
@@ -0,0 +1,60 @@
+const DogModel = require('../models/DogModel');
+const debug = require('../utils/debug');
+
+module.exports = {
+  /**
+   * Creates a new Dog document
+   *
+   * TODO: Implement
+   */
+  registerDog(req, res) {
+    throw new Error('not implemented yet!');
+  },
+
+  /**
+   * Returns one Dog by _id
+   *
+   * TODO: Implement
+   */
+  getDogById(req, res) {
+    throw new Error('not implemented yet!');
+  },
+
+  /**
+   * Returns a list of Dogs that a given user has yet to interact with
+   *
+   * TODO: Implement
+   */
+  getNearbyDogs(req, res) {
+    throw new Error('not implemented yet!');
+  },
+
+  /**
+   * Returns a list of Dogs that a given user has yet to interract with AND is
+   * filtered by either: distance (< default 50 mi), size, dog friendliness, and
+   * human friendliness
+   *
+   * TODO: Implement
+   */
+  getDogsWithFilter(req, res) {
+    throw new Error('not implemented yet!');
+  },
+
+  /**
+   * Updates a single Dog by _id with new information
+   *
+   * TODO: Implement
+   */
+  updateDogById(req, res) {
+    throw new Error('not implemented yet!');
+  },
+
+  /**
+   * Deletes a single Dog by _id
+   *
+   * TODO: Implement
+   */
+  deleteDogById(req, res) {
+    throw new Error('not implemented yet!');
+  },
+};
---
+++ server/models/DogModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const dogSchema = new mongoose.Schema({
+  name: String,
+  breed: String,
+  mainImageUrl: String,
+  imageUrls: [String],
+  energy: String, // low, medium, high
+  size: String, // small, medium, large
+  isDogFriendly: String,
+  isHumanFriendly: String,
+  bio: String,
+  friendsList: [String],
+  pendingDogs: [String],
+  rejectedDogs: [String],
+  eventsAttending: [String],
+  eventsPending: [String],
+  location: {
+    address1: String,
+    address2: String,
+    city: String,
+    state: String,
+    postalCode: String,
+    latitude: Number,
+    longitude: Number,
+  },
+  owner: {
+    name: String,
+    imageUrl: String,
+  },
+  userInfo: {
+    email: String,
+    password: String,
+  },
+});
+
+const DogModel = mongoose.model('Dog', dogSchema);
+
+module.exports = DogModel;
