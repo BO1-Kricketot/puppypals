6ba35c059e897eee1c1a340bcfb006e8cb23d29a
Jacob Fink <jacobifink@gmail.com>
2023-03-07 14:17:48 -0800

    Added more functionality to adding pictures.

--- client/features/LogInSignUp/CreateProfile.js
+++ client/features/LogInSignUp/CreateProfile.js
@@ -2,6 +2,7 @@ import {
   Button,
   StyleSheet,
   Text,
+  Image,
   SafeAreaView,
   TextInput,
   ScrollView,
@@ -15,6 +16,7 @@ const CreateProfile = () => {
   const { height } = useWindowDimensions();
   const [dogName, setDogName] = useState('');
   const [dogBreed, setDogBreed] = useState('');
+  const [dogProfPic, setDogProfPic] = useState();
   const [dogPics, setDogPics] = useState([]);
   const [energyLvl, setEnergyLvl] = useState();
   const [size, setSize] = useState();
@@ -22,18 +24,38 @@ const CreateProfile = () => {
   const [humanFriendliness, setHumanFriendliness] = useState();
   const [bio, setBio] = useState('');
   const [ownerName, setOwnerName] = useState('');
-  const [ownerPic, setOwnerPic] = useState({});
+  const [ownerPic, setOwnerPic] = useState();
 
-  const pickImage = async () => {
+  const pickOwnerImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      aspect: [3, 4],
+      aspect: [7, 10],
       quality: 1,
-      base64: true,
+      // base64: true,
     });
 
     console.log(result);
+
+    if (!result.canceled) {
+      setOwnerPic(result.assets[0].uri);
+    }
+  };
+
+  const pickDogProfImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [7, 10],
+      quality: 1,
+      // base64: true,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setDogProfPic(result.assets[0].uri);
+    }
   };
 
   return (
@@ -52,6 +74,17 @@ const CreateProfile = () => {
           style={styles.inputs}
         />
         {/* insert pic upload here after finding out how */}
+        <Text>Select your dog's profile picture</Text>
+        <Button
+          title="Pick an image from camera roll"
+          onPress={pickDogProfImage}
+        />
+        {dogProfPic && (
+          <Image
+            source={{ uri: dogProfPic.toString() }}
+            style={{ width: 200, height: 200 }}
+          />
+        )}
         <Picker
           selectedValue={energyLvl}
           onValueChange={(e) => setEnergyLvl(e)}>
@@ -66,7 +99,8 @@ const CreateProfile = () => {
         </Picker>
         <Picker
           selectedValue={dogFriendliness}
-          onValueChange={(e) => setDogFriendliness(e)}>
+          onValueChange={(e) => setDogFriendliness(e)}
+          style={styles.dropdown}>
           <Picker.Item label="Not Friendly" value={false} />
           <Picker.Item label="Friendly" value={true} />
         </Picker>
@@ -88,7 +122,17 @@ const CreateProfile = () => {
           placeholder="Enter Your Name"
           style={styles.inputs}
         />
-        <Button title="Pick an image from camera roll" onPress={pickImage} />
+        <Text>Select your profile picture</Text>
+        <Button
+          title="Pick an image from camera roll"
+          onPress={pickOwnerImage}
+        />
+        {ownerPic && (
+          <Image
+            source={{ uri: ownerPic.toString() }}
+            style={{ width: 200, height: 200 }}
+          />
+        )}
       </SafeAreaView>
     </ScrollView>
   );
@@ -96,7 +140,7 @@ const CreateProfile = () => {
 
 const styles = StyleSheet.create({
   root: {
-    alignItems: 'center',
+    // alignItems: 'center',
     width: '100%',
   },
   logo: {
@@ -105,11 +149,15 @@ const styles = StyleSheet.create({
     maxHeight: 200,
   },
   inputs: {
-    width: '80%',
+    width: '100%',
     borderWidth: 1,
     borderRadius: 5,
     padding: 3,
     marginVertical: 3,
+    alignItems: 'center',
+  },
+  dropdown: {
+    // width: 'auto',
   },
   button: {
     borderWidth: 1,
