2b5040029d5a227b8c28e6b9cadf451662eabffd
Pseudo08 <arpan.shrestha57@gmail.com>
2023-03-08 10:21:12 -0800

    Cleanup

--- client/App.js
+++ client/App.js
@@ -1,18 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Example from './features/Example';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeLanding from './features/Home/HomeLanding.jsx';
 
 import Profile from './features/Profile';
 
-const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
     <><HomeLanding/></>
-    
   );
 }
 
