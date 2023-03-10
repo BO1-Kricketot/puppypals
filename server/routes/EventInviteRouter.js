a5f933ed489eb21ea154bccec5c76f570747311c
boltonlin <boltonlinbo@gmail.com>
2023-03-10 11:07:22 -0800

    added delete router for event invites

--- server/routes/EventInviteRouter.js
+++ server/routes/EventInviteRouter.js
@@ -3,5 +3,6 @@ const controller = require('../controllers').eventInvite;
 
 EventInviteRouter.post('/', controller.sendEventInvite);
 EventInviteRouter.get('/:recipientId', controller.getEventInvitesById);
+EventInviteRouter.delete('/:eventId', controller.deleteEventInviteById);
 
 module.exports = EventInviteRouter;
